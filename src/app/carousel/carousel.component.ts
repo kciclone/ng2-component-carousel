d31888e226a91f84a1896826def34ffcb04d0e8e
Lieven VG <lievenvg@users.noreply.github.com>
2017-04-24 17:04:37 +0200

    Fully working carousel. Only active indicator not set yet.

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class CarouselComponent {
 
   items = [
-    {id: 0, source: "http://placehold.it/350x150/d19c8a/d19c8a"},
-    {id: 1, source: "http://placehold.it/350x150/d1c887/d1c887"},
-    {id: 2, source: "http://placehold.it/350x150/a6d18a/a6d18a"},
-    {id: 3, source: "http://placehold.it/350x150/86d1b5/86d1b5"},
-    {id: 4, source: "http://placehold.it/350x150/d186c2/d186c2"}
+    {source: "http://placehold.it/350x150/d19c8a/d19c8a"},
+    {source: "http://placehold.it/350x150/d1c887/d1c887"},
+    {source: "http://placehold.it/350x150/a6d18a/a6d18a"},
+    {source: "http://placehold.it/350x150/86d1b5/86d1b5"},
+    {source: "http://placehold.it/350x150/d186c2/d186c2"}
   ]
 
   public currentItemId = 0;
@@ -23,21 +23,27 @@ export class CarouselComponent {
     let parsedPositionValue = "";
     positionValue = positionValue * this.currentItemId*100;
     parsedPositionValue = "-" + positionValue + "%";
-    console.log("setPosition parsedPositionValue executed:" + parsedPositionValue);
     return parsedPositionValue;
   }
 
 
-  public setItem = (item) => {
-    // let a = item.indexOf(this.currentItem );
-    if (this.currentItemId === item.id) return;
-    this.currentItemId = item.id;
-    console.log("setItem currentItemId executed:" + this.currentItemId);
+  public setItem = (item,items) => {
+    if (this.currentItemId === items.indexOf(item)) return;
+    this.currentItemId = items.indexOf(item);
+    return this.currentItemId;
+  }
+  public next(){
+    if (this.currentItemId === this.items.length - 1) return;
+    this.currentItemId = this.currentItemId + 1;
     return this.currentItemId;
   }
 
+  public previous(){
+    if (this.currentItemId === 0) return;
+    this.currentItemId = this.currentItemId - 1;
+    return this.currentItemId;
+  }
 
-  
   public getPanesWidth() {
     let baseWidth = 100;
     let newWidth = '';
