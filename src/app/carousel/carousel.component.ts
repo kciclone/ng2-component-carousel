5708615bcb4b460a8f2461271a7fa41f234f6223
Tom Opdebeeck <tom230691@gmail.com>
2017-05-09 12:11:58 +0200

    update component
    * use lifecycle hooks to update props over calling functions from the template (ngOnInit & ngOnChanges)
    * next + previous = move
    * use template literals to shorten string concatenation

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -9,6 +9,8 @@ import {
   OnChanges,
   SimpleChanges
 } from '@angular/core';
+import * as get from 'lodash/get';
+
 import { CarouselItem } from './carousel.types';
 
 @Component({
@@ -17,8 +19,7 @@ import { CarouselItem } from './carousel.types';
   styleUrls: ['./carousel.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush // only re-render component if inputs have changed, see https://blog.thoughtram.io/angular/2016/02/22/angular-2-change-detection-explained.html
 })
-export class CarouselComponent {
-
+export class CarouselComponent implements OnInit, OnChanges {
   // see https://angular.io/docs/ts/latest/cookbook/component-communication.html
   @Input() items: CarouselItem[] = []; // expect an array of CarouselItems from the parent component
   @Output() move = new EventEmitter(); // expose an event handler to the parent
@@ -26,48 +27,48 @@ export class CarouselComponent {
   // see first 2 answers (docs are missing) http://stackoverflow.com/questions/37965647/hostbinding-and-hostlistener-what-do-they-do-and-what-are-they-for
   @HostBinding('class.carousel-container') carouselContainerClass = true; // set a class on the host element (carousel)
 
-  selectedItem = 0;
-  
+  public selectedItem = 0;
+  public itemWidth = '';
+  public totalWidth = '';
+  public position = '';
+
+  // see https://angular.io/docs/ts/latest/guide/lifecycle-hooks.html for more info on lifecycle hooks
+
+  // do something the first time the component renders
+  ngOnInit() {
+    this.totalWidth = `${100 * this.items.length}%`;
+    this.itemWidth = `${100 / this.items.length}%`;
+    this.setPosition();
+  }
+
+  // do something when the input changes
+  ngOnChanges(changes: SimpleChanges) {
+    const newItems = get(changes, 'items.newValue', null);
+
+    if (newItems) {
+      this.totalWidth = `${100 * this.items.length}%`;
+      this.itemWidth = `${100 / this.items.length}%`;
+    }
+  }
 
   public setPosition(){
-    let positionValue = 1;
-    let parsedPositionValue = "";
-    positionValue = positionValue * this.selectedItem*100;
-    parsedPositionValue = "-" + positionValue + "%";
-    return parsedPositionValue;
+    // see https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Template_literals
+    this.position = `-${this.selectedItem * 100}%`;
   }
 
-
-  public setItem = (index) => {
-    if (this.selectedItem === index) return;
+  public setItem(index: number): void {
     this.selectedItem = index;
-    return  this.selectedItem;
-  }
-  public next(){
-    if (this.selectedItem === this.items.length - 1) return;
-    this.selectedItem = this.selectedItem + 1;
-    return this.selectedItem;
+    this.setPosition();
+    this.move.emit(this.selectedItem); // emit the move event, see https://toddmotto.com/component-events-event-emitter-output-angular-2
   }
 
-  public previous(){
-    if (this.selectedItem === 0) return;
-    this.selectedItem = this.selectedItem - 1;
-    return this.selectedItem;
-  }
+  public moveItem(factor: number = 0): void {
+    const newIndex = this.selectedItem + factor;
 
-  public getPanesWidth() {
-    let baseWidth = 100;
-    let newWidth = '';
-    baseWidth = baseWidth * this.items.length;
-    newWidth = baseWidth + '%';
-    return newWidth;
-  }
+    if (newIndex < 0 || newIndex === this.items.length) {
+      return;
+    }
 
-  public getLiWidth() {
-    let baseWidth = 100;
-    let newWidth = '';
-    baseWidth = baseWidth / this.items.length;
-    newWidth = baseWidth + '%';
-    return newWidth;
-  }   
+    this.setItem(newIndex);
+  }
 }
